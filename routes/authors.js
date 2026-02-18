332841923f94d771f17b70f78fa9f368044fe9dd
Michael Persson <mpersson@pratt.edu>
2026-02-17 20:19:14 -0500

    Moved authors to their own file

--- routes/authors.js
+++ routes/authors.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const authors = [
-  {firstName: "James", lastName: "S. A. Corey"},
-  {firstName: "Craig", lastName: "Alanson"},
-  {firstName: "Cixin", lastName: "Liu"},
-  {firstName: "Eugene", lastName: "Levy"}
-  ]
-
 router.get('/', function(req, res, next) {
 
   res.render('authors/index', { title: 'BookedIn || Authors', authors: authors });
