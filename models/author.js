79ec21f33321a97c0880f01705196076e1030ed6
Michael Persson <mpersson@pratt.edu>
2026-02-17 20:19:37 -0500

    Now importing author.js from /model

---
+++ models/author.js
@@ -0,0 +1,8 @@
+const authors = [
+  {firstName: "James", lastName: "S. A. Corey"},
+  {firstName: "Craig", lastName: "Alanson"},
+  {firstName: "Cixin", lastName: "Liu"},
+  {firstName: "Eugene", lastName: "Levy"}
+  ]
+
+  exports.all = authors
--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const Author = require('../models/author');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
 
+  const authors = Author.all;
   res.render('authors/index', { title: 'BookedIn || Authors', authors: authors });
 });
 
