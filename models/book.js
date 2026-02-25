080bdae3d41a043f99a893ea369adb7e82ab280f
Michael Persson <mpersson@pratt.edu>
2026-02-24 19:20:08 -0500

    Updated Books view

--- models/book.js
+++ models/book.js
@@ -1,9 +1,12 @@
 const books = [
   {title: "Leviathan Wakes", publishingYear: 2011},
   {title: "Caliban's War", publishingYear: 2012},
+  {title: "Millenium Dictionary", publishingYear:2000},
   ]
 
-  exports.all = books;
+
   exports.add = (book) => {
   books.push(book);
 }
+
+  exports.all = books
