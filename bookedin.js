8ff80cf69f29f00da7852b2ca12f1b9df19891d7
Michael Persson <mpersson@pratt.edu>
2026-03-10 02:40:50 -0400

    Updated router

--- bookedin.js
+++ bookedin.js
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/authors', authorsRouter);
 app.use('/books', booksRouter);
-app.use('/genres');
+app.use('/genres', genresRouter);
 
 
 app.use((_req, res) => {
